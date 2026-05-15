0252348893fdebff3871d239fd69f607b436e78a
Anup Sapkota <np.anup123@gmail.com>
2026-05-15 18:35:32 +0800

    API setup

---
+++ api/contact.js
@@ -0,0 +1,26 @@
+import { Resend } from "resend";
+
+const resend = new Resend("re_S8qJC5gu_JSEwc7x8vbbvjWSsqVzKfeDm");
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { name, email, message } = req.body;
+
+  try {
+    await resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: 'np.anup123@gmail.com',
+      subject: "New Portfolio Message",
+      html: `
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b> ${message}</p>
+      `,
+    });
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
